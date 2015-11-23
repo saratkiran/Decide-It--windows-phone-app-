3da0e7d2970001940d480582355b896450581a81
Sarat kiran Andhavarapu <saratkiran18@gmail.com>
2015-11-22 23:05:06 -0600

    Decide It (Windows Phone App)
    Decide It (Windows Phone App)

---
+++ Decisions/DecisionType.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Decisions
+{
+    public class DecisionType
+    {
+        public static IEnumerable<DecisionType> GetDecisionTypes()
+        {
+            return new[]
+            {
+                new DecisionType
+                {
+                    Name = "\n\t Yes / No",
+                    Choices = new[]
+                    {
+                        "Yes", "No"
+                    }
+                },
+                new DecisionType
+                {
+                    Name = "\n\t A / B / C / D",
+                    Choices = new[]
+                    {
+                        "A", "B", "C", "D"
+                    }
+                },
+                new DecisionType
+                {
+                    Name = "\n\t1 - 100",
+                    Choices = Enumerable.Range(1, 100).Select(i => i.ToString())
+                },
+                new DecisionType
+                {
+                    Name = "\n\tLeft / Right",
+                    Choices = new[]
+                    {
+                        "Left", "Right"
+                    }
+                },
+                new DecisionType
+                {
+                    Name = "\n\tUp / Down",
+                    Choices = new[]
+                    {
+                        "Up", "Down"
+                    }
+                },
+
+                new DecisionType
+                {
+                    Name = "\n\tBuy / Sell / Hold",
+                    Choices = new[]
+                    {
+                        "Buy", "Sell", "Hold"
+                    }
+                },
+                new DecisionType
+                {
+                    Name = "\n\tAgree / Disagree",
+                    Choices = new[]
+                    {
+                        "Agree", "Disagree"
+                    }
+                },
+                new DecisionType
+                {
+                    Name = "\n\tHeads / Tails",
+                    Choices = new[]
+                    {
+                        "Heads", "Tails"
+                    }
+                },
+                
+            };
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public IEnumerable<string> Choices
+        {
+            get; set;
+        }
+    }
+}
---
+++ Decisions/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace Decisions
+{
+    public partial class MainPage
+    {
+        private readonly Random random = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Decisions.ItemsSource = DecisionType.GetDecisionTypes();
+        }
+
+        private void OnDecide(object sender, RoutedEventArgs e)
+        {
+            if(Decisions.SelectedItem == null)
+                return;
+
+            var decisionType = (DecisionType)Decisions.SelectedItem;
+
+            var decision = decisionType.Choices.ElementAt(random.Next(decisionType.Choices.Count()));
+
+            MessageBox.Show(decision, "The decision is", MessageBoxButton.OK);
+        }
+    }
+}
